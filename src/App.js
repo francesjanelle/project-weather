3694b3c84afd905d879d3de5a3a66bd232b5ea3a
Frances-Janelle-Torres <frances.janelle.torres@gmail.com>
2021-01-26 15:02:07 -0700

    Update
    Currently there is an error but working on it

--- src/App.js
+++ src/App.js
@@ -3,5 +3,13 @@ import React from "react"
 import './index.css'
 
 export default function App() {
-  return <div className="App">Weather App </div>; 
+  return 
+  <div className="App">
+    Weather App
+    <footer> 
+      <p>
+        <a href="https://github.com/francesjanelle/project-weather" target="_blank">GitHub Repsitory</a>
+      by Frances-Janelle Torres</p>
+    </footer>
+  </div>; 
 };
