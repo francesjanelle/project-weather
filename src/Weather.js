e748f0c42af97a039525157fd29eeaabf2b2fd69
Frances-Janelle-Torres <frances.janelle.torres@gmail.com>
2021-01-27 12:40:56 -0700

    Update

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,10 @@
 import React /*, { useState } */ from "react"
+// import axios from "react-axios"
 import "./index.css"
 
 export default function Weather(props) {
+    function current() {
+        
+    }
     return <div> <h2>"Hello World"</h2> </div>
 }
