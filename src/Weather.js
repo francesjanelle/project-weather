a27092a17e67ed740f3ea86b230a549f17e81b4b
Frances-Janelle Torres <frances.janelle.torres@gmail.com>
2021-01-30 11:21:19 -0700

    Update

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,30 @@
-import React /*, { useState } */ from "react"
+import React, { useState } from "react"
 // import axios from "react-axios"
 import "./index.css"
 
 export default function Weather(props) {
-    
-    return <div> <h2>"Hello World"</h2> </div>
+    const [setCity, city] = useState("")
+
+    function WeatherShow(response) { 
+        console.log(response)
+    }
+
+
+    function WeatherSearch(event) {
+        event.preventDefault()
+      //  const apiKey = "deb4d0036edfa966c7a36750fd024ceb"
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=deb4d0036edfa966c7a36750fd024ceb&units=metric`
+        axios.get(apiUrl).then(WeatherShow)
+    }
+
+    function WeatherUpdate() { 
+
+    }
+
+    return ( <div className="weather-form">
+        <form onSubmit={WeatherSearch}>
+            <input type="search" placeholder="Location" onChange={WeatherUpdate} /> {" "}
+            <input type="submit" value="Search"/>
+        </form>
+    </div>)
 }
