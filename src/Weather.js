5a8fad530e2bdb0ddedbf66a1a766357fb47718c
Frances-Janelle Torres <frances.janelle.torres@gmail.com>
2021-01-30 19:25:38 -0700

    Update

--- src/Weather.js
+++ src/Weather.js
@@ -1,60 +1,64 @@
-import React , { useState } from "react"
-import axios from "axios"
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-import Time from "./Time"
-import "./index.css"
+import React, { useState } from "react";
+import axios from "axios";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Time from "./Time";
+import "./index.css";
 
 export default function Weather(props) {
-    const [setCity, city] = useState("");
-    const [setTemp, temp] = useState("");
+    const [setWeatherData, weatherData] = useState({ ready: false });
 
     function WeatherShow(response) { 
         console.log(response.data)
+
     }
 
+    if (ready) {
 
-    function WeatherSearch(event) {
-        event.preventDefault()
+        return (
+            <div className="weather-form">
+                <div className="container">
+                <h1> Denver </h1>
+                    <Time />
+                    <hr/> 
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h2> 20° </h2>
+                                <h3> Clear Skies </h3>
+                            </Col>
+                            <Col>
+                                <ul>
+                                    <li> 💧  Precipitation:  </li>
+                                    <li> 🎐  Humidity</li>
+                                </ul>
+                            </Col>
+                            <Col>
+                                <form>
+                                    <input type="search" placeholder="Location" /* onChange={WeatherUpdate} */ /> {" "}
+                                    <Button variant="primary" type="submit">Search</Button>
+                                </form>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <hr />
+                </div>
+        </div>)
+    } else {
+        
       //  const apiKey = "deb4d0036edfa966c7a36750fd024ceb"
-        let city = "Denver"
-        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=deb4d0036edfa966c7a36750fd024ceb&units=metric`
+      //   let city = "Denver"
+        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=deb4d0036edfa966c7a36750fd024ceb&units=metric`
         axios.get(apiUrl).then(WeatherShow)
+
+        return "Hold on.."
     }
 
   /*  function WeatherUpdate(event) { 
         setCity(event.target.value)
     } */
 
-    return (
-        <div className="weather-form">
-            <div className="container">
-            <h1> Denver </h1>
-                <Time />
-                <hr/> 
-                <Container>
-                    <Row>
-                        <Col>
-                            <h2> 20° </h2>
-                            <h3> Clear Skies </h3>
-                        </Col>
-                        <Col>
-                            <ul>
-                                <li> 💧  Precipitation:  </li>
-                                <li> 🎐  Humidity</li>
-                            </ul>
-                        </Col>
-                        <Col>
-            <form onSubmit={WeatherSearch}>
-                <input type="search" placeholder="Location" /* onChange={WeatherUpdate} */ /> {" "}
-                <Button variant="primary" type="submit">Search</Button>
-                            </form>
-                            </Col>
-                        </Row>
-                </Container>
-                <hr />
-            </div>
-    </div>)
+    
 }
