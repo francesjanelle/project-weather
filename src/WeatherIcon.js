6433e12fac94b65e52b6c975be23e57a9030ab83
Frances-Janelle-Torres <frances.janelle.torres@gmail.com>
2021-02-06 12:08:02 -0700

    update

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,10 +2,10 @@ import React from "react"
 
 export default function WeatherIcon(props) {
     const codeMap = {
-        
+
     }
 
-    return
+    return ffffff
      <img
         src={props.info.icon}
         alt={props.info.description} />
