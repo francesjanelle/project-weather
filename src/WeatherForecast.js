e66fe6b9df64b7f179c1527b4bb4c08124de89f6
Frances-Janelle-Torres <frances.janelle.torres@gmail.com>
2021-02-18 22:04:13 -0700

    update

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react"
+// import WeatherIcon from "./WeatherIcon"
+import ForecastPreview from "./WeatherForecastPreview"
 import axios from "axios"
 
 export default function WeatherForecast(props) {
@@ -6,17 +8,21 @@ export default function WeatherForecast(props) {
     const [forecast, setForecast] = useState(null)
 
     function ForecastHandle(response) {
-        setForecast(response.data)
+        setForecast(response.info)
         setLoaded(true)
     }
     
     if (loaded) { 
- //       console.log(forecast.list)
-    }
+  //      console.log(forecast.list[0])
+        return (
+            <ForecastPreview props={forecast.list[0]}/>
+            )
+    } else {
 
    //  const key = deb4d0036edfa966c7a36750fd024ceb
     let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=deb4d0036edfa966c7a36750fd024ceb&units=metric`
     axios.get(apiUrl).then(ForecastHandle)
 
-    return <h2>{props.city}</h2>
- }
+    return null; 
+    }
+}
--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,5 +1,17 @@
-import React, { useState } from React
+import React, { useState } from "react"
+import WeatherIcon from "./WeatherIcon"
 
-export default function ForecastPreview(orops) { 
-    
+export default function ForecastPreview(props) { 
+    return ( <div className="forecast">
+                {new Date(props.data.dt * 1000).getHours()}:00
+                <WeatherIcon code={props.list[0].weather[0].icon} />
+                {Math.round(props.list[0].main.temp)} 
+            </div>)
 }
+
+/* <div className="forecast">
+                {new Date(forecast.list[0].dt * 1000).getHours()}:00
+                <WeatherIcon code={forecast.list[0].weather[0].icon} />
+                {Math.round(forecast.list[0].main.temp)} 
+            </div>
+            */
